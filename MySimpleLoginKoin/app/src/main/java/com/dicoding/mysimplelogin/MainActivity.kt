6c620899874823684310dbb6120000ef877f0cca
yysrll <yusril.uzumaki@gmail.com>
2022-07-01 08:59:40 +0800

    Dependency Injection practice with koin

---
+++ MySimpleLoginKoin/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.dicoding.mysimplelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
+import org.koin.android.ext.android.inject
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val userRepository: UserRepository by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        we dont need it again
+//        val sesi = SessionManager(this)
+//        userRepository = UserRepository.getInstance(sesi)
+
+        if (userRepository.isUserLogin()) {
+            moveToHomeActivity()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            saveSession()
+        }
+    }
+
+    private fun saveSession() {
+        userRepository.loginUser(binding.edUsername.text.toString())
+        moveToHomeActivity()
+    }
+
+    private fun moveToHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+}
