53741cf7df2cadead730d0e9f02b8a39031f34fd
yysrll <yusril.uzumaki@gmail.com>
2022-07-03 11:41:35 +0800

    Dependency Injection practice with Dagger

---
+++ MySimpleLoginDagger/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.dicoding.mysimplelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    @Inject
+    lateinit var userRepository: UserRepository
+    @Inject
+    lateinit var userRepository2: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        (application as MyApplication).appComponent.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val sesi = SessionManager(this)
+//        userRepository = UserRepository.getInstance(sesi)
+        userRepository.checkInstance()
+        userRepository2.checkInstance()
+
+        if (userRepository.isUserLogin()) {
+            moveToHomeActivity()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            saveSession()
+        }
+    }
+
+    private fun saveSession() {
+        userRepository.loginUser(binding.edUsername.text.toString())
+        moveToHomeActivity()
+    }
+
+    private fun moveToHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+}
